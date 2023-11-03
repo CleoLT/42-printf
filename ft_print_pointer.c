8432064abd594811666eb582c176c396e5626751
Cleo Le Tron <ale-tron@car13s4.42barcelona.com>
2023-11-03 08:10:16 +0100

    refactoring print pointer

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: ale-tron <ale-tron@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 11:52:49 by ale-tron          #+#    #+#             */
-/*   Updated: 2023/11/01 20:00:17 by ale-tron         ###   ########.fr       */
+/*   Updated: 2023/11/03 08:09:08 by ale-tron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
@@ -14,18 +14,17 @@
 static int	ft_long_hex(unsigned long num)
 {
 	int		count;
-	char	hex;
+	int		hex;
+	char	*base;
 
-	count = 1;
+	base = "0123456789abcdef";
+	count = 0;
 	if (num >= 16)
-		count += ft_long_hex(num / 16);
-	if (num % 16 > 9)
-		hex = num % 16 - 10 + 'a';
-	if (num % 16 < 10)
-		hex = num % 16 + '0';
-	if (write(1, &hex, 1) == -1)
+		count = ft_long_hex(num / 16);
+	if (count == -1)
 		return (-1);
-	return (count);
+	hex = ft_print_char(base[num % 16]);
+	return (count + hex);
 }
 
 int	ft_print_pointer(unsigned long address)
